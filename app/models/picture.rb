316361a93b369f77078e94301b9d8911c89005bd
kazukiyuyama <b00513266@essec.edu>
2015-06-20 21:20:49 +0900

    fixe picture.rb

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,10 +1,6 @@
 class Picture < ActiveRecord::Base
   belongs_to :attachable_image, polymorphic: true
-  has_attached_file :image, :storage => :dropbox,
-:dropbox_credentials => "#{Rails.root}/config/dropbox_config.yml",:styles => { :medium => "300x300>" },
-:dropbox_options => {       
-:path => proc { |style| "#{Rails.env}/#{style}/#{id}_#{picture.original_filename}"}, :unique_filename => true 
-  }
+  has_attached_file :image, :styles => { :medium => "300x300>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   default_scope -> { order('created_at ASC') }
   def preview
